7f670d9c728de71a4af4fff6040053561d4233d9
Geir Hedemark <geir@hedemark.net>
2023-08-12 23:11:35 +0200

    starting to generate column descriptions

--- lib/src/main/kotlin/org/testaco/database/schema/SchemaVerifier.kt
+++ lib/src/main/kotlin/org/testaco/database/schema/SchemaVerifier.kt
@@ -2,12 +2,10 @@ package org.testaco.database.schema
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import org.junit.jupiter.api.fail
-import org.testaco.IgnoredTable
-import org.testaco.Table
-import org.testaco.TestacoSchema
-import org.testaco.TestacoTable
 import java.sql.DatabaseMetaData
+import java.sql.ResultSet
+import org.junit.jupiter.api.fail
+import org.testaco.*
 
 object SchemaVerifier {
     private val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
@@ -19,14 +17,16 @@ object SchemaVerifier {
     ) {
         databaseMetaData.getTables(null, null, null, arrayOf<String>("TABLE")).use { resultSet ->
             while (resultSet.next()) {
-                val tableName: String = resultSet.getString("TABLE_NAME")
-                    ?: fail("Don't know how to cope if metadata does not return tablename, panic!")
+                val tableName: String =
+                    resultSet.getString("TABLE_NAME")
+                        ?: fail("Don't know how to cope if metadata does not return tablename, panic!")
                 val schema: String? = resultSet.getString("TABLE_SCHEM")
                 val remarks: String? = resultSet.getString("REMARKS")
 
-                val referenceTable: TestacoTable? = referenceSchema.tables.find { it.tableName == tableName }
+                val referenceTable: TestacoTable? =
+                    referenceSchema.tables.find { it.tableName == tableName }
                 when (referenceTable) {
-                    is IgnoredTable -> {} //Do nothing, we need to ignore this
+                    is IgnoredTable -> {} // Do nothing, we need to ignore this
                     is Table -> println("Table: $tableName $schema $remarks")
                     else -> {
                         fail(
@@ -34,7 +34,8 @@ object SchemaVerifier {
                                 |Reference schema $schemaFileName does not contain a definition for table $tableName
                                 |Options for declaration are 
                                 |${examples(tableName, databaseMetaData)}
-                                |""".trimMargin()
+                                |"""
+                                .trimMargin()
                         )
                     }
                 }
@@ -46,13 +47,26 @@ object SchemaVerifier {
         return """
             ${ignoredExample(tableName, metadata)}
             ${tableExample(tableName, metadata)}
-            """.trimIndent()
+            """
+            .trimIndent()
     }
 
-    private fun ignoredExample(tableName: String, metadata: DatabaseMetaData): String = mapper.writeValueAsString(
-        IgnoredTable(tableName)
-    )
-    private fun tableExample(tableName: String, metadata: DatabaseMetaData): String = mapper.writeValueAsString(
-        Table(tableName, emptyList())
-    )
+    private fun ignoredExample(tableName: String, metadata: DatabaseMetaData): String =
+        mapper.writeValueAsString(IgnoredTable(tableName))
+    private fun tableExample(tableName: String, metadata: DatabaseMetaData): String {
+        val columns: List<TestacoColumn> =
+            metadata.getColumns(null, null, tableName, null).use { columns ->
+                generateSequence {
+                    while (columns.next()) {
+                        val columnName = columns.getString("COLUMN_NAME")
+                        val dataType = columns.getInt("DATA_TYPE")
+                        val dataTypeName = columns.getString("TYPE_NAME")
+                        val isAutoIncrement = columns.getString("IS_AUTOINCREMENT")
+                        val isGeneratedColumn = columns.getString("IS_GENERATEDCOLUMN")
+                        yield TestacoColumn(columnName)
+                    }
+                }.toList()
+            }
+        return mapper.writeValueAsString(Table(tableName, emptyList()))
+    }
 }
