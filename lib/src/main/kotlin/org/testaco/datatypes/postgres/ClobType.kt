5380e0bdbe31fdbbb5591d46e0f3e123b806fad9
Geir Hedemark <geir@hedemark.net>
2023-08-20 11:39:39 +0200

    Clob type

---
+++ lib/src/main/kotlin/org/testaco/datatypes/postgres/ClobType.kt
@@ -0,0 +1,27 @@
+package org.testaco.datatypes.postgres
+
+import org.testaco.datatypes.TestacoType
+import java.io.StringReader
+import java.sql.Clob
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.Types
+
+open class ClobType : TestacoType<String?> {
+  override fun read(columnName: String, rs: ResultSet): String? {
+    val i: Clob = rs.getClob(columnName)
+    return if (rs.wasNull()) {
+      null
+    } else {
+      i.characterStream.readText()
+    }
+  }
+
+  override fun store(value: String?, columnIndex: Int, statement: PreparedStatement) {
+    if (value == null) {
+      statement.setNull(columnIndex, Types.CLOB)
+    } else {
+      statement.setClob(columnIndex, StringReader(value))
+    }
+  }
+}
